e1b083f2b04e02c8939e89a5054f83339d9bdcdd
Eric Sales De Andrade <eric.salesdeandrade@joinstudyhall.com>
2023-08-16 11:43:07 +0530

    Add more sorting algo

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,5 +12,36 @@ def input_data_large():
 
 
 @pytest.fixture
-def bubble_sort_input1():
+def sort_input1():
     return [5, 1, 4, 2, 8, 9, 3, 7, 6, 0]
+
+
+@pytest.fixture
+def sort_input2():
+    return [
+        5,
+        1,
+        4,
+        2,
+        8,
+        9,
+        3,
+        7,
+        6,
+        0,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        0,
+        -3,
+        -6,
+        4,
+        1,
+        9,
+        6,
+        10,
+    ]
--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,25 +1,77 @@
 import pytest
-
 from src.math_examples import calculate_sum
-from src.sorting_examples import bubble_sort
+from src.sorting_examples import bubble_sort, quick_sort, insertion_sort
 
 
-@pytest.mark.math
+@pytest.mark.math_small
 def test_calculate_sum_small(input_data_small, benchmark):
     result = benchmark(calculate_sum, input_data_small)
     assert result == sum(input_data_small)
 
 
-@pytest.mark.math
+@pytest.mark.math_small
+def test_inbuilt_sum_small(input_data_small, benchmark):
+    result = benchmark(sum, input_data_small)
+    assert result == sum(input_data_small)
+
+
+@pytest.mark.math_large
 def test_calculate_sum_large(input_data_large, benchmark):
     result = benchmark(calculate_sum, input_data_large)
     assert result == sum(input_data_large)
 
 
-@pytest.mark.sorting
-def test_bubble_sort_small(bubble_sort_input1, benchmark):
-    result = benchmark(bubble_sort, bubble_sort_input1)
-    assert result == sorted(bubble_sort_input1)
+@pytest.mark.math_large
+def test_inbuilt_sum_large(input_data_large, benchmark):
+    result = benchmark(sum, input_data_large)
+    assert result == sum(input_data_large)
 
 
-# Write more sorting algorithms and test/benchmark them here.
+# Sorting - Small
+@pytest.mark.sort_small
+def test_bubble_sort_small(sort_input1, benchmark):
+    result = benchmark(bubble_sort, sort_input1)
+    assert result == sorted(sort_input1)
+
+
+@pytest.mark.sort_small
+def test_quick_sort_small(sort_input1, benchmark):
+    result = benchmark(quick_sort, sort_input1)
+    assert result == sorted(sort_input1)
+
+
+@pytest.mark.sort_small
+def test_insertion_sort_small(sort_input1, benchmark):
+    result = benchmark(insertion_sort, sort_input1)
+    assert result == sorted(sort_input1)
+
+
+@pytest.mark.sort_small
+def test_inbuilt_sort_small(sort_input1, benchmark):
+    result = benchmark(sorted, sort_input1)
+    assert result == sorted(sort_input1)
+
+
+# Sorting - Large
+@pytest.mark.sort_large
+def test_bubble_sort_large(sort_input2, benchmark):
+    result = benchmark(bubble_sort, sort_input2)
+    assert result == sorted(sort_input2)
+
+
+@pytest.mark.sort_large
+def test_quick_sort_large(sort_input2, benchmark):
+    result = benchmark(quick_sort, sort_input2)
+    assert result == sorted(sort_input2)
+
+
+@pytest.mark.sort_large
+def test_inbuilt_sort_large(sort_input2, benchmark):
+    result = benchmark(sorted, sort_input2)
+    assert result == sorted(sort_input2)
+
+
+@pytest.mark.sort_large
+def test_insertion_sort_large(sort_input2, benchmark):
+    result = benchmark(insertion_sort, sort_input2)
+    assert result == sorted(sort_input2)
