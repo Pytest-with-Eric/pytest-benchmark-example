eb6d314e7aa0e87efbbc9efff3cd029974bb2ceb
Eric Sales De Andrade <sdaeric19@gmail.com>
2023-08-24 12:31:54 +0530

    Not using the math examples

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,16 +2,6 @@ import pytest
 
 
 @pytest.fixture
-def input_data_small():
-    return list(range(100))
-
-
-@pytest.fixture
-def input_data_large():
-    return list(range(10000))
-
-
-@pytest.fixture
 def sort_input1():
     return [5, 1, 4, 2, 8, 9, 3, 7, 6, 0]
 
